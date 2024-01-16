2bd35b83a03e26423704bb1efd734527228977f5
emilyzinnia <ezhang@physics.utoronto.ca>
2024-01-15 22:50:11 -0500

    move p_ outside of try block

--- src/stack.jl
+++ src/stack.jl
@@ -102,13 +102,13 @@ function read_lattice_stack(file::String)
     f = h5open(file, "r") 
     paramsfile = read(attributes(f)["paramsfile"])
     lockname = lock_file(paramsfile) # lock file
+    p_ = h5open(paramsfile, "r")
     try
-        h5open(paramsfile, "r") do p_
-            lat = read_lattice(p_) 
-        end
+        lat = read_lattice(p_) 
         return lat
     finally
         close(f)
+        close(p_)
         unlock_file(lockname) #unlock file
     end
 end
