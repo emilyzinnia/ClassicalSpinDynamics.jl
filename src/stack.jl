c9e936717765f8bfab371cd4a84fb3bb2c4cbc31
emilyzinnia <ezhang@physics.utoronto.ca>
2024-01-15 20:27:53 -0500

    close lockfilehandle in lock file function

--- src/stack.jl
+++ src/stack.jl
@@ -6,13 +6,13 @@ using ClassicalSpinMC: read_lattice
 Pushes file to end of stack 
 """
 function pushToStack!(stackfile, file)
-    lockhandle, lockname = lock_file(stackfile) # lock file
+    lockname = lock_file(stackfile) # lock file
     try
         open(stackfile, "a") do io # write line to end stack 
             write(io, "$file\n")
         end
     finally
-        unlock_file(lockhandle, lockname) # unlock file 
+        unlock_file(lockname) # unlock file 
     end
 end
 
@@ -22,7 +22,7 @@ Reads first line from stackfile and creates new stackfile with first line remove
 function pullFromStack!(stackfile::String)::String
     file = []
     sleep_rand(10) # sleep to prevent threads from accessing at the same time 
-    lockhandle, lockname = lock_file(stackfile) # lock file
+    lockname = lock_file(stackfile) # lock file
     (tmppath, tmpio) = mktemp(dirname(stackfile)) # make a temporary stackfile
     try
         open(stackfile) do io
@@ -35,7 +35,7 @@ function pullFromStack!(stackfile::String)::String
         return file[1]
     finally
         close(tmpio)
-        unlock_file(lockhandle, lockname) # unlock file
+        unlock_file(lockname) # unlock file
     end
 end
 
@@ -57,11 +57,16 @@ function lock_file(sharefilename::String)
     lockfilename = lockfile_location * basename(sharefilename) * ".lock"
     local lockfilehandle 
     while !lockacquired
+        attempt = 1 
         while isfile(lockfilename)
             # watch_file will notify if the file status changes, waiting until then
             # here we want to wait for the file to get deleted
             println("$sharefilename locked, idling...")
-            watch_file(lockfilename, 20.0) # timeout after 20 seconds 
+            watch_file(lockfilename, 10.0) # timeout after 10 seconds 
+            attempt += 1 
+            if attempt > 5 #force remove lockfile if idling too many times 
+                rm(lockfilename, force=true)
+            end 
         end
         try
             # try to acquire the lock by creating lock file with JL_O_EXCL (exclusive)
@@ -76,17 +81,18 @@ function lock_file(sharefilename::String)
             else
                 rethrow()
             end
+        finally 
+            close(lockfilehandle)
         end
     end
-    return lockfilehandle, lockfilename
+    return lockfilename
 end
 
 """
 Frees up lock to shared file. 
 """
-function unlock_file(lockfilehandle, lockfilename)
+function unlock_file(lockfilename)
     # free up the lock so that the other process can acquire it if it needs
-    close(lockfilehandle)
     Filesystem.unlink(lockfilename)
 end
 
@@ -96,13 +102,14 @@ Reads file from stack and returns lattice object (threadsafe).
 function read_lattice_stack(file::String)
     f = h5open(file, "r") 
     paramsfile = read(attributes(f)["paramsfile"])
-    lockhandle, lockname = lock_file(paramsfile) # lock file
-    p_ = h5open(paramsfile, "r")
+    lockname = lock_file(paramsfile) # lock file
     try
-        return read_lattice(p_) 
+        h5open(paramsfile, "r") do p_
+            lat = read_lattice(p_) 
+        end
+        return lat
     finally
         close(f)
-        close(p_)
-        unlock_file(lockhandle, lockname) #unlock file
+        unlock_file(lockname) #unlock file
     end
 end
